293b292751b932e16f267c578c630ea0c71c0e02
Sam Wood <samwooddev@gmail.com>
2021-02-02 21:53:27 +0000

    Project submission

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
-
-# Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=sam72/devops
 
 # Step 2:  
-# Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag devops $dockerpath
 
 # Step 3:
-# Push image to a docker repository
+docker push $dockerpath
